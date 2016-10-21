91aa9f01ba7ba92a9aaa31bfdc42af1386c2e92e
pmatveeva <polink.matveeva@yandex.ru>
2016-10-21 17:39:58 +0300

    Update decorators.py

--- librip/decorators.py
+++ librip/decorators.py
@@ -1,18 +1,12 @@
 def print_result(func):
-    def decorated_func(*args):
+    def decorated_func(*args, **kwargs):
         print(func.__name__)
-        if len(args)>0:
-            res=func(args[0])
-        else:
-            res=func()
+        res=func(*args, **kwargs)
         if type(res) is list:
             print("\n".join(map(str,res)))
         elif type(res) is dict:
             print('\n'.join([str(x)+"="+str(res[x]) for x in res]))
         else:
             print(res)
-        return (res)
+        return res
     return decorated_func
-
-
-
